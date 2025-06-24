c6ad0df69712d054868ca3891bdcd4faf8204713
Emerson Matias <emerson.matias@betalent.tech>
2025-06-24 15:37:31 -0300

    feat: add Section4

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import HeroSection from '@/components/sections/hero-section'
 import Section1 from '@/components/sections/section-1'
 import Section2 from '@/components/sections/section-2'
 import Section3 from '@/components/sections/section-3'
+import Section4 from '@/components/sections/section-4'
 
 export default function HomePt() {
   return (
@@ -10,6 +11,7 @@ export default function HomePt() {
       <Section1 />
       <Section2 />
       <Section3 />
+      <Section4 />
     </main>
   )
 }
